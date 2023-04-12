d421ea3c97037c32f0b502c0e84eebad5bda01f8
Ali <amsaar.1416@gmail.com>
2023-04-13 01:18:16 +0200

    Added predicate (D) and update (E)

--- P1.pl
+++ P1.pl
@@ -4,12 +4,23 @@ ta_slot_assignment([ta(HT,HN)|TT],RemTAs,Name):-
     HN1 >= 0,
 	RemTAs = [ta(HT,HN1)|TT].
 	
-ta_slot_assignment([ta(HT,_)|TT],[HT|TR],Name):-
+ta_slot_assignment([ta(HT,HTN)|TT],[ta(HT,HTN)|TR],Name):-
 	HT \= Name,!,
 	ta_slot_assignment(TT,TR,Name).
 
 %Edone
-
+slot_assignment(LabsNum,TAs,RemTAs,Assignment):-
+	permutation(TAs,P),
+	slot_assignment_helper(LabsNum,P,P,RemTAs,Assignment).
+	
+slot_assignment_helper(0,_,RemTAs,RemTAs,[]).	
+	
+slot_assignment_helper(LabsNum,TAs,RemTAsT,RemTAs,Assignment):-
+	LabsNumNew is LabsNum-1,
+	TAs = [ta(HN,_)|TTT],
+	ta_slot_assignment(RemTAsT,RemTAsNEW,HN),
+	slot_assignment_helper(LabsNumNew,TTT,RemTAsNEW,RemTAs,TAssignment),
+	Assignment = [HN | TAssignment].
 %DDone
 max_slots_per_day(DaySched,Max):-
     maxSlot_helper1(DaySched,TAs),
