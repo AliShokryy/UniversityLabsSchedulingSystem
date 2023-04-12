ecb3603b933e9e5d4113f80aba10c09baa2bc9ae
Ali <amsaar.1416@gmail.com>
2023-04-12 17:36:24 +0200

    Added predicate (e)

--- P1.pl
+++ P1.pl
@@ -1,1 +1,10 @@
-%AAA%
+ta_slot_assignment([ta(HT,HN)|TT],RemTAs,Name):-
+	HT = Name,!,
+    HN1 is HN-1,
+    HN1 >= 0,
+	RemTAs = [ta(HT,HN1)|TT].
+	
+ta_slot_assignment([ta(HT,_)|TT],[HT|TR],Name):-
+	HT \= Name,!,
+	ta_slot_assignment(TT,TR,Name).
+
