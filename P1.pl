c259e51d92db125d90326469152a56b8f9f158af
Talaat-jr <abdelrahmanmt96@gmail.com>
2023-04-13 00:55:12 +0200

    Finished predicate C

--- P1.pl
+++ P1.pl
@@ -8,3 +8,29 @@ ta_slot_assignment([ta(HT,_)|TT],[HT|TR],Name):-
 	HT \= Name,!,
 	ta_slot_assignment(TT,TR,Name).
 
+%Edone
+
+%DDone
+max_slots_per_day(DaySched,Max):-
+    maxSlot_helper1(DaySched,TAs),
+    maxSlot_helper2(TAs,DaySched,Max).
+
+maxSlot_helper1([],[]).
+maxSlot_helper1([H|T],TAs):- 
+    maxSlot_helper1(T,L1),
+    union(H,L1,TAs).
+maxSlot_helper2([],_,_).
+maxSlot_helper2([H|T],DaySched,Max):-
+    maxSlot_helper3(H,DaySched,0,Max),
+    maxSlot_helper2(T,DaySched,Max).
+maxSlot_helper3(_,[],_,_).
+maxSlot_helper3(E,[H|T],Count,Max):-
+    Count=<Max,
+    member(E,H),!,
+    Count1 is Count+1,
+    maxSlot_helper3(E,T,Count1,Max).
+maxSlot_helper3(E,[H|T],Count,Max):-
+    Count=<Max,
+    \+member(E,H),
+    maxSlot_helper3(E,T,Count,Max).    
+%Cdone
